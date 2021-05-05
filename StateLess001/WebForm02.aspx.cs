9b557d90d69694e966761013e4556978b32c777d
PedroAvila <minimainor_austin@hotmail.com>
2021-05-05 05:39:42 -0500

    ViewState

---
+++ StateLess001/WebForm02.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateLess001
+{
+    public partial class WebForm02 : System.Web.UI.Page
+    {
+        /// <summary>
+        /// ViewState nos permite preservar los datos entre requests
+        /// El ViewState viaja con la petición y la respuesta, de esa manera se conserva el dato
+        /// Tenemos que colocar el nombre de la variable para el ViewState
+        /// Si no se ha usado previamente tiene el valor null
+        /// Cuando lo leamos es importante hacer el typecast correspondiente
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+
+        private int _conteo = 1;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtConteo.Text = "0";
+            }
+        }
+
+        protected void btnIncrementa_Click(object sender, EventArgs e)
+        {
+            if (ViewState["click"]!= null)
+            {
+                _conteo = (int)ViewState["click"] + 1;
+            }
+            txtConteo.Text = _conteo.ToString();
+            ViewState["click"] = _conteo;
+        }
+    }
+}
---
+++ StateLess001/WebForm03.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateLess001
+{
+
+    public partial class WebForm03 : System.Web.UI.Page
+    {
+        /// <summary>
+        /// Los controles ASP.NET usan internamente a ViewState para guardar su estado
+        /// Pero esto no nos quita de la responsabilidad de guardar los datos
+        /// Correctamente en el ViewState
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtConteo.Text = "0";
+            }
+        }
+
+        protected void btnIncrementa_Click(object sender, EventArgs e)
+        {
+            int conteo = Convert.ToInt32(txtConteo.Text) + 1;
+            txtConteo.Text = conteo.ToString();
+        }
+    }
+}
