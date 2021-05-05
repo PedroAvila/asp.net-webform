1e508fd73333172ae55960df487e2d248cc26e2b
PedroAvila <minimainor_austin@hotmail.com>
2021-05-05 04:46:00 -0500

    Introducción y estado en la aplicación

---
+++ StateLess001/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateLess001
+{
+    public partial class _Default : Page
+    {
+        private int _conteo = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verificamos que no sea un posback del webform
+            if (!IsPostBack)
+            {
+                txtConteo.Text = "0";
+            }
+        }
+
+        protected void btnIncrementa_Click(object sender, EventArgs e)
+        {
+            _conteo++;
+            txtConteo.Text = _conteo.ToString();
+        }
+    }
+}
